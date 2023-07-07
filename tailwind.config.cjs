c15e0b49bd358d02f0b01aa4dd0d898311055e81
ricafolio <29043464+ricafolio@users.noreply.github.com>
2023-07-07 19:43:18 +0800

    chore: extend tailwind fontFamily

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"switzer": ["Switzer", "sans-serif"],
+				"panchang": ["Panchang", "sans-serif"],
+			}
+		},
 	},
 	plugins: [],
 }
